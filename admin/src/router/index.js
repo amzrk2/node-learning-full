f8a793cf81e5d321193f35591e941dd93e90c2a4
amzrk2 <amzrk2@outlook.com>
2020-07-19 11:58:48 +0800

    管理员登陆界面初始化

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -6,6 +6,11 @@ Vue.use(VueRouter);
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+  },
+  {
     path: '/',
     name: 'Main',
     component: Main,
@@ -89,14 +94,6 @@ const routes = [
         props: true,
       },
     ],
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
---
+++ admin/src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <el-card class="login-card" header="管理员登录">
+      <el-form @submit.native.prevent="handleLogin">
+        <el-form-item label="用户名">
+          <el-input v-model="model.username" />
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="model.password" />
+        </el-form-item>
+        <el-form-item class="login-btn">
+          <el-button type="primary" native-type="submit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      model: {
+        username: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    handleLogin() {
+      console.log(JSON.stringify(this.model));
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-card {
+  margin-top: 5rem;
+  width: 90%;
+  max-width: 30rem;
+}
+
+.login-btn {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+
+  ::v-deep .el-button {
+    width: 100%;
+  }
+}
+</style>
