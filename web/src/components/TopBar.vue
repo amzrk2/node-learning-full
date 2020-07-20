1dd1137093f89bcd6514a9de6dff0284ea0bbf43
amzrk2 <amzrk2@outlook.com>
2020-07-20 14:45:28 +0800

    Topbar 和 Navbar 完工

---
+++ web/src/components/TopBar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="topbar color-bg-dark">
+    <div class="topbar-wrapper d-flex">
+      <img src="../assets/logo.png" />
+      <div class="topbar-title">
+        <h1 class="color-white font-md">王者荣耀</h1>
+        <p class="color-white font-xxs">团队成就更多</p>
+      </div>
+      <div class="flex-grow"></div>
+      <div class="download color-bg-primary">
+        <a
+          class="download-btn text-center font-sm"
+          href="//pvp.qq.com/zlkdatasys/mct/d/play.shtml"
+        >立即下载</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBar'
+};
+</script>
+
+<style lang="scss" scoped>
+.topbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 1rem;
+  z-index: 20;
+  display: block;
+  padding: 0.1875rem 0.4rem;
+}
+
+.topbar-wrapper {
+  height: 100%;
+  width: 100%;
+
+  img {
+    display: block;
+    width: 0.65rem;
+  }
+}
+
+.topbar-title {
+  padding-left: 0.2rem;
+
+  h1 {
+    font-weight: 400;
+    margin: 0;
+  }
+
+  p {
+    margin-top: 0.1rem;
+    color: #666;
+  }
+}
+
+.download {
+  border-radius: 0.05rem;
+  width: 1.35rem;
+  height: 0.55rem;
+
+  a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    line-height: 0.55rem;
+  }
+}
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,29 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Main from '../views/Main.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Main', // 首页架构
+    component: Main,
+    children: [
+      {
+        path: '/',
+        name: 'Home', // 主页内容
+        component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+      },
+      {
+        path: '/strategy',
+        name: 'Strategy', // 攻略中心
+        component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+      },
+      {
+        path: '/match',
+        name: 'Match', // 赛事中心
+        component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+      },
+    ],
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
