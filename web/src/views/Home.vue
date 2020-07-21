de14b5a9e48844a77f96b995317ba2d4bf11ebe5
amzrk2 <amzrk2@outlook.com>
2020-07-21 13:04:12 +0800

    卡片封装

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,18 +2,34 @@
   <div class="home">
     <ad-swiper />
     <nav-icons />
+    <slot-card :card-data="cardData1">啊啊啊</slot-card>
+    <slot-card :card-data="cardData2">不不不</slot-card>
   </div>
 </template>
 
 <script>
 import AdSwiper from '@/components/home/AdSwiper.vue';
 import NavIcons from '@/components/home/NavIcons.vue';
+import SlotCard from '@/components/SlotCard.vue';
 
 export default {
   name: 'Home',
   components: {
     AdSwiper,
-    NavIcons
+    NavIcons,
+    SlotCard
+  },
+  data() {
+    return {
+      cardData1: {
+        title: '新闻资讯',
+        icon: 'spirit xw'
+      },
+      cardData2: {
+        title: '英雄列表',
+        icon: 'spirit hero'
+      }
+    };
   }
 };
 </script>
