690311c73bbad4d102a57cdf505f304113b0b033
amzrk2 <amzrk2@outlook.com>
2020-07-25 13:49:59 +0800

    记录文章录入时间

--- server/model/Article.js
+++ server/model/Article.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  title: { type: String },
-  // 类型为ID，关联Category模型
-  category: { type: mongoose.SchemaTypes.ObjectId, ref: 'Category' },
-  body: { type: String },
-});
+const schema = new mongoose.Schema(
+  {
+    title: { type: String },
+    // 类型为ID，关联Category模型
+    category: { type: mongoose.SchemaTypes.ObjectId, ref: 'Category' },
+    body: { type: String },
+  },
+  {
+    timestamps: true, // 记录时间
+  }
+);
 
 module.exports = mongoose.model('Article', schema);
