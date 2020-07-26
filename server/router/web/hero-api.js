78196396e288cbd1cb2ea02f354c4fb311b6b1e0
amzrk2 <amzrk2@outlook.com>
2020-07-26 15:24:40 +0800

    详情页 BANNER 与技能模块完工

--- server/router/web/hero-api.js
+++ server/router/web/hero-api.js
@@ -3,6 +3,20 @@ const mongoose = require('mongoose');
 const Hero = mongoose.model('Hero');
 
 module.exports = async function articleApi(req, res) {
-  const hero = await Hero.findById(req.params.id);
+  const hero = await Hero.findById(req.params.id)
+    .populate('categories')
+    .populate('itemsAdv')
+    .populate('itemsDis')
+    .lean();
+  // 后处理英雄分类
+  if (hero.categories.length > 1) {
+    let temp = [];
+    hero.categories.forEach((cat) => {
+      temp.push(cat.name);
+    });
+    hero.categories = temp.join('/');
+  } else {
+    hero.categories = hero.categories[0].name;
+  }
   res.send(hero);
 };
--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -1,7 +1,53 @@
 <template>
   <div class="hero color-bg-bg">
     <top-bar />
-    <div class="content"></div>
+    <div class="content">
+      <div class="banner">
+        <span class="title font-sm">{{ heroData.title }}</span>
+        <span class="name font-xxl">{{ heroData.name }}</span>
+        <span class="categories font-sm">{{ heroData.categories }}</span>
+        <div class="scores font-sm">
+          <span class="difficult">
+            难度
+            <i>{{ heroData.scores.difficult }}</i>
+          </span>
+          <span class="skills">
+            技能
+            <i>{{ heroData.scores.skills }}</i>
+          </span>
+          <span class="attack">
+            攻击
+            <i>{{ heroData.scores.attack }}</i>
+          </span>
+          <span class="survive">
+            生存
+            <i>{{ heroData.scores.survive }}</i>
+          </span>
+        </div>
+      </div>
+      <div class="skills">
+        <div class="skill-tab">
+          <div
+            :class="['skill-icon', { active: index === activeSkill }]"
+            v-for="(item, index) of heroData.skills"
+            :key="`icon-${item._id}`"
+            @click="activeSkill = index"
+          >
+            <img :src="item.icon" />
+          </div>
+        </div>
+        <div
+          class="skill-content"
+          v-show="index === activeSkill"
+          v-for="(item, index) of heroData.skills"
+          :key="`content-${item._id}`"
+        >
+          <span class="skill-name">{{ item.name }}</span>
+          <span class="skill-des">{{ item.description }}</span>
+          <span class="skill-tips">小提示：{{ item.tips }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +62,13 @@ export default {
   components: { TopBar },
   data() {
     return {
-      heroData: {},
+      activeSkill: 0,
+      heroData: {
+        title: '',
+        name: '',
+        categories: '',
+        scores: { difficult: 0, skills: 0, attack: 0, survive: 0 },
+      },
     };
   },
   methods: {
@@ -36,5 +88,105 @@ export default {
 <style lang="scss" scoped>
 .content {
   margin-top: 0.9rem;
+}
+
+div.skills {
+  background-color: map-get($colors, 'white');
+
+  .skill-tab {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 0.4rem;
+    padding: 0.4rem;
+
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  .skill-icon {
+    border-radius: 50%;
+    border: 0.05rem solid transparent;
+
+    &.active {
+      border: 0.05rem solid map-get($colors, 'primary');
+    }
+  }
+
+  .skill-content {
+    padding: 0 0.2rem 0.2rem 0.2rem;
+
+    .skill-name {
+      display: block;
+      font-size: 0.3rem;
+      font-weight: bold;
+    }
+
+    .skill-des {
+      display: block;
+      margin-top: 0.3rem;
+    }
+
+    .skill-tips {
+      display: block;
+      margin-top: 0.3rem;
+      color: map-get($colors, 'grey');
+    }
+  }
+}
+
+.banner {
+  height: 50vw;
+  position: relative;
+  color: map-get($colors, 'white');
+  background-color: #636363;
+
+  & > span {
+    position: absolute;
+    left: 0.3rem;
+    right: 0.3rem;
+  }
+
+  .title {
+    bottom: 1.45rem;
+  }
+
+  .name {
+    bottom: 0.96rem;
+  }
+
+  .categories {
+    bottom: 0.65rem;
+  }
+
+  .scores {
+    position: absolute;
+    bottom: 0.3rem;
+    left: 0.3rem;
+    right: 0.3rem;
+
+    i {
+      font-style: normal;
+      display: inline-block;
+      width: map-get($font-sizes, 'md');
+      height: map-get($font-sizes, 'md');
+      text-align: center;
+      border-radius: 50%;
+    }
+
+    & > span:nth-child(1) i {
+      background-color: #7f5300;
+    }
+    & > span:nth-child(2) i {
+      background-color: #1f3695;
+    }
+    & > span:nth-child(3) i {
+      background-color: #781a16;
+    }
+    & > span:nth-child(4) i {
+      background-color: #33332c;
+    }
+  }
 }
 </style>
